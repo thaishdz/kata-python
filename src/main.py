611249fc8be8b199e7f1eb5b7d2eb6bac8b81d0b
thaisdev <thaishdzlpz@gmail.com>
2025-08-01 14:02:04 +0100

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,6 +1,64 @@
-def start():
-    raise NotImplementedError("Method not implemented.")
+"""
+Se te proporciona el punto de inicio inicial (x, y) de un rover y la dirección (N, S, E, W) 
+en la que está orientado.
+El rover recibe una matriz de caracteres con comandos.
+Implementa comandos que muevan el rover hacia adelante/atrás (f, b).
+Implementa comandos que giren el rover a la izquierda/derecha (l, r).
+
+"""
+
+class Rover:
+
+    def __init__(self):
+        self.position_x = 0
+        self.position_y = 0
+        self.orientation = 'N'
+
+    def process_commands(self,commands: list[str]): # F,R,F,F,L
+
+        for command in commands:
+            if command == 'F' or command == 'B':
+                self.move(command)
+            elif command == 'R' or command == 'L':
+                self.rotate(command)
+            else:
+                raise "CommandNotFound"
+
+    def move(self, movement: str): 
+        if movement == 'F':
+            self.position_y += 1
+        elif movement == 'B':
+            self.position_y -= 1
+        
+    def rotate(self,direction: str):
+
+        if self.orientation == 'N' and direction == 'R':
+            self.orientation = 'E'
+            return 
+        elif direction == 'L':
+            self.orientation = 'W'
+            return 
+
+        if self.orientation == 'E' and direction == 'R':
+            self.orientation = 'S'
+            return
+        elif direction == 'L':
+            self.orientation = 'N'
+            return
+        if self.orientation == 'S' and direction == 'R':
+            self.orientation = 'W'
+            return
+        elif direction == 'L':
+            self.orientation = 'E'
+            return
+        if self.orientation == 'W' and direction == 'R':
+            self.orientation = 'N'
+            return
+        elif direction == 'L':
+            self.orientation = 'S'
+            return
+
 
 
 if __name__ == "__main__":
-    start()
+    Rover()
--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,8 +1,48 @@
 import pytest
 
-from src.main import start
+from src.main import Rover
 
 
-def test_main_start():
-    with pytest.raises(NotImplementedError):
-        start()
+def test_rover_init():
+    rover = Rover()
+    assert rover.position_x == 0
+    assert rover.position_y == 0
+    assert rover.orientation == 'N'
+
+def test_proccess_commands():
+    rover = Rover()
+    commands = ['F','R','F','F','L']
+    rover.process_commands(commands)
+    assert rover.position_x == 2
+    assert rover.position_y == 1
+    assert rover.orientation == 'S'
+
+def test_move_forward():
+    rover = Rover()
+    rover.move('F')
+    assert rover.position_x == 0
+    assert rover.position_y == 1
+    assert rover.orientation == 'N'
+
+
+def test_move_backward():
+    rover = Rover()
+    rover.move('B')
+
+    assert rover.position_x == 0
+    assert rover.position_y == -1
+    assert rover.orientation == 'N'
+
+
+def test_rotate_left():
+    rover = Rover()
+    rover.rotate('L')
+
+    assert rover.orientation == 'W'
+
+
+def test_rotate_right():
+    rover = Rover()
+    rover.rotate('R')
+
+    assert rover.orientation == 'E'
